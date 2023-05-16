aea05e97ab461721250e0c7f50f99220ae39dafb
nksb <65872605+nksb@users.noreply.github.com>
2023-05-16 22:33:20 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'NKSB Workspaces',
+    description: 'Development area for new Kasm Workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://nksb.github.io/kasm/',
+    contactUrl: 'https://nksb.github.io/kasm/',
   },
   reactStrictMode: true,
   swcMinify: true,
